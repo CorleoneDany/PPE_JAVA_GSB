ab5bad07fd1df776c0e773eb55db878cd7c34783
Matthias Carmona <matthias.carmona@m2sformation.com>
2020-04-17 16:48:20 +0200

    Implementation de la class Main

--- src/controleur/Main.java
+++ src/controleur/Main.java
@@ -5,6 +5,13 @@
  */
 package controleur;
 
+import javax.swing.JOptionPane;
+import modele.jdbc.Jdbc;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+import modele.jdbc.FabriqueJdbc;
+import modele.jdbc.Jdbc;
 /**
  *
  * @author Nero
@@ -15,7 +22,42 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Variable contenant le nom du fichier properties
+        String ficPropertiesJdbc = "gsbJdbc.properties";
+        Ctrlprincipal ctrlprincipal;
+        
+        if(args.length > 0){
+            ficPropertiesJdbc = args[0];
+        }
+        
+        // Singleton de connexion Jdbc
+        try {
+          FabriqueJdbc.creer(ficPropertiesJdbc);
+        }
+        catch(FileNotFoundException ex){
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "Lanceur Main - Fichier de propriétés JDBC introuvable", JOptionPane.ERROR_MESSAGE);
+        }
+        catch(IOException ex){
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "Lanceur Main - Erreur lors du chargement du fichier de propriétés JDBC", JOptionPane.ERROR_MESSAGE);
+        }
+        
+        // Ouverture de la connection avec Jdbc
+        try{
+            Jdbc.getInstance().connecter();
+        }
+        catch(ClassNotFoundException ex){
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "Lanceur Main - connexion à la BDD - pilote JDBC non trouvé", JOptionPane.ERROR_MESSAGE);
+        }
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "Lanceur Main - connexion à la BDD", JOptionPane.ERROR_MESSAGE);
+        }
+        
+        // Lancement de l'application instancier par la class CtrlPrincipal
+        ctrlPrincipal = new CtrlPrincipal();
+        ctrlPrincipal.action();
+        
+        
+        
     }
     
 }
