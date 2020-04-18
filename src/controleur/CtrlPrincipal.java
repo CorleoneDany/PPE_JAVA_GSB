fec5f35c7619a5d7d0ece689692a0a875d5aa84c
François ESPIASSE <franco1s.espiassec@gmail.com>
2020-04-18 22:04:02 +0200

    CtrlPrincipal.java : Ajout des attributs et de la méthode action()

--- src/controleur/CtrlPrincipal.java
+++ src/controleur/CtrlPrincipal.java
@@ -6,9 +6,22 @@
 package controleur;
 
 /**
- *
  * @author François ESPIASSE
  */
 public class CtrlPrincipal {
-       
+
+    private CtrlVisiteur ctrlVisiteur = null;
+    private CtrlMenu     ctrlMenu     = null;
+
+    /**
+     * Utilise par défaut le contrôleur principal
+     * Affiche la vue du menu
+     */
+    public void action() {
+        if (ctrlMenu == null) ctrlMenu = new CtrlMenu(this);
+        
+        ctrlMenu.getVue().setEnabled(true);
+        ctrlMenu.getVue().setVisible(true);
+    }
+   
 }
